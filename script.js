096bd76d4f8532a7ea1992a23472cefc12a6b548
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-06-27 15:55:14 +0400

    added field validation and finished styling

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+const passwordInput = document.querySelector("#passwordInput")
+const confirmPasswordInput = document.querySelector("#confirmPasswordInput")
+const submitButton = document.querySelector("#submitBtn")
+const errorText = document.querySelector("#errorText")
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    var password = passwordInput.value;
+    var confirmPassword = confirmPasswordInput.value;
+
+    console.log(password)
+    console.log(confirmPassword)
+  
+    if (password !== confirmPassword) {
+        errorText.classList.remove("hidden")
+      return;
+    }else{
+        errorText.classList.add("hidden")
+    }
+    event.target.submit();
+  });
